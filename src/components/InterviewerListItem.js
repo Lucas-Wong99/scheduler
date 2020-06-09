d3b3fc0240abb724b9a2590e218600574d882dc4
Lucas <lucaswong.ca@gmail.com>
2020-06-08 20:22:38 -0700

    Added interviewer list item and styling

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './InterviewerListItem.scss';
+import classNames from 'classnames';
+
+export default function InterviewerListItem(props) {
+  const { name, avatar, selected, setInterviewer} = props;
+
+  const interviewerClass = classNames(
+    "interviewers__item",
+    {
+      "interviewers__item--selected": selected,
+    }
+  );
+
+  const interviewerImgClass = classNames(
+    "interviewers__item",
+    "interviewers__item-image",
+    {
+      "interviewers__item--selected-image": selected,
+    }
+  );
+  
+  return (
+    <li className={interviewerClass} onClick={()=> setInterviewer(name)}>
+  <img
+    className={interviewerImgClass}
+    src={avatar}
+    alt={name}
+  />
+  {name}
+</li>
+  );
+}
