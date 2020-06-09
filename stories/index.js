1d3aef858e6687cbe37766b635fa5f9d9e14058a
Lucas <lucaswong.ca@gmail.com>
2020-06-09 16:38:15 -0700

    Filled out Form and tested with stories

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -164,5 +165,21 @@ storiesOf("Appointment", module)
     <Error 
       message="Could not delete appointment"
       onClose={action("onClose")}
+    />
+  ))
+  .add("Form Edit", () => (
+    <Form 
+      name="Lucas"
+      interviewers={interviewers}
+      interviewer={4}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form Create", () => (
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ))
