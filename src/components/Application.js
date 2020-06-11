9c2700217b8fb799bc30638e23b4530742f46fe8
Lucas <lucaswong.ca@gmail.com>
2020-06-10 17:51:52 -0700

    Combines states into one object, and added functions to handle state chagnes

--- src/components/Application.js
+++ src/components/Application.js
@@ -54,8 +54,15 @@ const appointments = [
 ];
 
 export default function Application() {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prevState => ({...prevState, days}));
 
   useEffect(() => {
     axios.get("/api/days")
@@ -82,8 +89,8 @@ export default function Application() {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
