b7bd89b4c5c6aa46c0b4f210433d2a231c3c6252
Lucas <lucaswong.ca@gmail.com>
2020-06-10 20:23:54 -0700

    Created getInterview selector and implemented it in Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 import "components/Application.scss";
 
@@ -12,7 +12,8 @@ export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -20,22 +21,31 @@ export default function Application() {
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ])
-    .then(([resDays, resAppointments]) => {
+    .then(([resDays, resAppointments, resInterviewers]) => {
       setState(prevState => ({
         ...prevState,
         days: resDays.data,
-        appointments: resAppointments.data
+        appointments: resAppointments.data,
+        interviewers: resInterviewers.data
       }))
     })
   }, []);
 
-  const appointmentsArr = getAppointmentsForDay(state, state.day).map((appointment) => {
-    return <Appointment
-      key={appointment.id}
-      {...appointment}
-    />
+  const appointments = getAppointmentsForDay(state, state.day);
+  const appointmentsArr = appointments.map((appointment) => {
+  const interview = getInterview(state, appointment.interview)
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+  )
   });
 
   return (
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -15,3 +15,26 @@ export function getAppointmentsForDay(state, day) {
 
   return res;
 }
+
+export function getInterview(state, interview) {
+  const { interviewers } = state;
+  
+  if (!interview) {
+    return null;
+  }
+
+  for (const key in interviewers) {
+    if (interviewers[key].id === interview.interviewer) {
+      return {
+        ...interview,
+        interviewer: interviewers[key]
+      }
+    }
+  }
+}
+
+//I;m given an interview(that currently holds a student name and interviewer id)
+// Want to return an object that replaces the interviewer id with a interviewer object
+// We also want to return null as default
+//check if given interview object interviewer id(for in through the interviewers object and check)
+//if id === id within given interview obj, replace the id with the current interveiwer obj
