fa638db23973d6c0eaacdf6b532b14009280013c
Lucas <lucaswong.ca@gmail.com>
2020-06-11 12:17:32 -0700

    Removed psuedo code from my selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -10,13 +10,12 @@ export function getAppointmentsForDay(state, day) {
   for (const key in appointments) {
     if(matchingDay.appointments.includes(appointments[key].id)) {
       res.push(appointments[key]);
-    }
+    }  
   }
-
   return res;
 }
 
-export function getInterview(state, interview) {
+export function getInterview(state, interview) { 
   const { interviewers } = state;
   
   if (!interview) {
@@ -32,9 +31,3 @@ export function getInterview(state, interview) {
     }
   }
 }
-
-//I;m given an interview(that currently holds a student name and interviewer id)
-// Want to return an object that replaces the interviewer id with a interviewer object
-// We also want to return null as default
-//check if given interview object interviewer id(for in through the interviewers object and check)
-//if id === id within given interview obj, replace the id with the current interveiwer obj
