35d16c6d50c3333fe025582338f65904a04abaff
Lucas <lucaswong.ca@gmail.com>
2020-06-14 12:17:15 -0700

    Bug fixes for update spots function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,7 +2,7 @@ import { useEffect, useReducer } from "react";
 import axios from "axios";
 
 const SET_DAY = "SET_DAY";
-const SET_DAYS = "SET_DAYS";
+// const SET_DAYS = "SET_DAYS";
 const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 const SET_INTERVIEW = "SET_INTERVIEW";
 
@@ -10,8 +10,8 @@ const SET_INTERVIEW = "SET_INTERVIEW";
     switch (action.type) {
       case SET_DAY:
         return {...state, day: action.day}
-      case SET_DAYS:
-        return {...state, days: action.days}
+      // case SET_DAYS:
+      //   return {...state, days: action.days}
       case SET_APPLICATION_DATA:
         return {
           ...state,
@@ -23,7 +23,7 @@ const SET_INTERVIEW = "SET_INTERVIEW";
         return {
           ...state,
           appointments: action.appointments,
-          // days: action.days(action.id, state.days, action.interview)
+          days: action.days(action.id, state.days, state.appointments, action.interview)
         }
       }
       default:
@@ -41,7 +41,7 @@ const useApplicationData = () => {
     appointments: {},
     interviewers: {}
   })
-  
+  console.log(state)
   const setDay = day => dispatch({ type: SET_DAY, day });
 
   useEffect(() => { 
@@ -59,12 +59,12 @@ const useApplicationData = () => {
     })
   }, []);
 
-  useEffect(() => {
-    axios.get("/api/days")
-    .then(res => {
-      dispatch({type: SET_DAYS, days: res.data})
-    }) 
-  })
+  // useEffect(() => {
+  //   axios.get("/api/days")
+  //   .then(res => {
+  //     dispatch({type: SET_DAYS, days: res.data})
+  //   }) 
+  // }, [state])
 
   // function updateSpotsForDay(id, days, interview) {
   //   return days.map((dayObj) => {
@@ -83,6 +83,29 @@ const useApplicationData = () => {
   //   })
   // };
 
+  function updateSpotsForDay(id, days, appointments, interview) {
+    return days.map((dayObj) => {
+      //checks if there is already an interview value for a specific appointment id
+      let interviewVal;
+      if (appointments[id].interview === null) {
+        interviewVal = null;
+      }
+      //checks if there appointment arr has the id, that the value of interview is not null, and if we are givin an interview object
+      if (dayObj.appointments.includes(id) && interviewVal !== null && typeof interview !== "undefined") {
+        return dayObj;
+      } else if (dayObj.appointments.includes(id)){
+        let spots = 0;
+          spots = (typeof interview !== "undefined"? dayObj.spots -= 1 : dayObj.spots += 1);
+          return { ...dayObj, spots }
+      } else {
+        return dayObj
+      }
+      
+    })
+  }
+
+  // console.log(updateSpotsForDay(1, state.days, state.appointments, {interview: 0}))
+
   function bookInterview(id, interview) {
     return axios.put(`/api/appointments/${id}`, {
         interview
@@ -99,9 +122,9 @@ const useApplicationData = () => {
       return dispatch({ 
         type: SET_INTERVIEW,
         appointments,
-        // id,
-        // days: updateSpotsForDay,
-        // interview
+        id,
+        days: updateSpotsForDay,
+        interview
       });     
     })
   }
@@ -120,8 +143,8 @@ const useApplicationData = () => {
       return dispatch({
         type: SET_INTERVIEW,
         appointments,
-        // id,
-        // days: updateSpotsForDay
+        id,
+        days: updateSpotsForDay
       });
     })
   }
